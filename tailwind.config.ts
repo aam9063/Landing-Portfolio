cfd32d863249d559823f2fb3e628ee855a9ef1c7
aam9063 <albert9063@gmail.com>
2024-12-13 15:46:18 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: "#52b788",
+        darkBg: "#1c1c1c",
+      },
+      backgroundImage: {
+        "gradient-cover":
+          "linear-gradient(90.21deg, rgba(28, 30, 30, 0.8) -5.91%, rgba(28, 30, 30, 0.8) 111.58%)",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
